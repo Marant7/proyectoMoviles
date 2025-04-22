1a7ddbdcbd14522eae1a4e192c92f2d91529534f
Andree Flores Melendez <af2017057494@virtual.upt.pe>
2025-04-22 02:50:32 -0500

    diseño juego1

--- lib/games/juego_1.dart
+++ lib/games/juego_1.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'juego_2.dart';
-
 import '../mensaje/celebracion.dart';
 
 class Question {
@@ -61,18 +60,8 @@ class _JuegoDragDropState extends State<JuegoDragDrop> {
     });
 
     if (allCorrect) {
-      Celebracion.mostrar(context); // <<<< Mostrar la celebración
+      Celebracion.mostrar(context);
     }
-
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(
-        content: Text(
-          allCorrect
-              ? '¡Todas las respuestas son correctas! 🎉'
-              : 'Hay respuestas incorrectas ❌',
-        ),
-      ),
-    );
   }
 
   void goToNextGame() {
@@ -85,66 +74,141 @@ class _JuegoDragDropState extends State<JuegoDragDrop> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Juego: Arrastra la palabra correcta')),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            ...List.generate(questions.length, (index) {
-              final q = questions[index];
-              final isImageLeft = index % 2 == 0;
-              return Padding(
-                padding: const EdgeInsets.symmetric(vertical: 12.0),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children:
-                      isImageLeft
-                          ? [
-                            imageBox(q.imagePath),
-                            Expanded(child: dropZone(index)),
-                            wordOptions(q.options, index),
-                          ]
-                          : [
-                            wordOptions(q.options, index),
-                            Expanded(child: dropZone(index)),
-                            imageBox(q.imagePath),
-                          ],
+      body: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color.fromARGB(255, 126, 168, 247), Color(0xFF8EC5FC)],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+        ),
+        child: SafeArea(
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                Text(
+                  'Juego: Arrastra la palabra correcta',
+                  style: TextStyle(
+                    fontSize: 26,
+                    fontWeight: FontWeight.bold,
+                    color: const Color.fromARGB(255, 26, 1, 104),
+                  ),
+                  textAlign: TextAlign.center,
                 ),
-              );
-            }),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: checkAnswers,
-              child: Text('Verificar respuestas'),
-            ),
-            SizedBox(height: 10),
-            if (isUnlocked)
-              ElevatedButton(
-                onPressed: goToNextGame,
-                style: ElevatedButton.styleFrom(backgroundColor: Colors.green),
-                child: Text('Siguiente juego'),
-              )
-            else
-              Opacity(
-                opacity: 0.4,
-                child: IgnorePointer(
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.grey,
+                SizedBox(height: 20),
+                ...List.generate(questions.length, (index) {
+                  final q = questions[index];
+                  final isImageLeft = index % 2 == 0;
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(20),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.black26,
+                            blurRadius: 6,
+                            offset: Offset(2, 4),
+                          ),
+                        ],
+                      ),
+                      padding: EdgeInsets.all(12),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children:
+                            isImageLeft
+                                ? [
+                                  imageBox(q.imagePath),
+                                  Expanded(child: dropZone(index)),
+                                  wordOptions(q.options, index),
+                                ]
+                                : [
+                                  wordOptions(q.options, index),
+                                  Expanded(child: dropZone(index)),
+                                  imageBox(q.imagePath),
+                                ],
+                      ),
                     ),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Icon(Icons.lock),
-                        SizedBox(width: 8),
-                        Text('Siguiente juego'),
-                      ],
+                  );
+                }),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: checkAnswers,
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color.fromARGB(255, 8, 10, 156),
+                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                  child: Text(
+                    'Verificar respuestas',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white, // <-- el color
                     ),
                   ),
                 ),
-              ),
-          ],
+                SizedBox(height: 20),
+                if (isUnlocked)
+                  ElevatedButton(
+                    onPressed: goToNextGame,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: const Color.fromARGB(255, 41, 134, 255),
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 30,
+                        vertical: 12,
+                      ),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                    child: Text(
+                      'Siguiente juego',
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  )
+                else
+                  Opacity(
+                    opacity: 0.5,
+                    child: IgnorePointer(
+                      child: ElevatedButton.icon(
+                        onPressed: () {},
+                        icon: Icon(Icons.lock),
+                        label: Text(
+                          'Siguiente juego',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            255,
+                            255,
+                            255,
+                          ),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 30,
+                            vertical: 12,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+              ],
+            ),
+          ),
         ),
       ),
     );
@@ -152,17 +216,16 @@ class _JuegoDragDropState extends State<JuegoDragDrop> {
 
   Widget imageBox(String imagePath) {
     return Container(
-      width: 100,
-      height: 100,
-      margin: EdgeInsets.symmetric(horizontal: 12),
+      width: 90,
+      height: 90,
+      margin: EdgeInsets.symmetric(horizontal: 8),
       decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey.shade300),
-        borderRadius: BorderRadius.circular(12),
-        color: Colors.white,
+        borderRadius: BorderRadius.circular(15),
+        color: Colors.grey.shade100,
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Image.asset(imagePath, fit: BoxFit.contain),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Image.asset(imagePath, fit: BoxFit.cover),
       ),
     );
   }
@@ -190,7 +253,6 @@ class _JuegoDragDropState extends State<JuegoDragDrop> {
 
   Widget dropZone(int questionIndex) {
     final answer = userAnswers[questionIndex];
-
     return DragTarget<String>(
       onAccept: (data) {
         setState(() {
@@ -200,16 +262,20 @@ class _JuegoDragDropState extends State<JuegoDragDrop> {
       builder: (context, candidateData, rejectedData) {
         return Container(
           height: 60,
-          margin: EdgeInsets.symmetric(horizontal: 16),
+          margin: EdgeInsets.symmetric(horizontal: 12),
           decoration: BoxDecoration(
-            color: Colors.green[50],
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(color: Colors.green),
+            color: Colors.deepPurple[50],
+            borderRadius: BorderRadius.circular(15),
+            border: Border.all(color: Colors.deepPurple),
           ),
           alignment: Alignment.center,
           child: Text(
             answer ?? 'Arrastra aquí',
-            style: TextStyle(fontSize: 18, color: Colors.black87),
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Colors.deepPurple[700],
+            ),
           ),
         );
       },
@@ -218,7 +284,7 @@ class _JuegoDragDropState extends State<JuegoDragDrop> {
 
   Widget wordChip(String word, {bool isDragging = false}) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 35, vertical: 6),
+      padding: EdgeInsets.symmetric(horizontal: 25, vertical: 8),
       decoration: BoxDecoration(
         color: isDragging ? Colors.orange[300] : Colors.orange[100],
         borderRadius: BorderRadius.circular(30),
@@ -227,13 +293,20 @@ class _JuegoDragDropState extends State<JuegoDragDrop> {
                 ? [
                   BoxShadow(
                     color: Colors.black26,
-                    blurRadius: 4,
-                    offset: Offset(2, 2),
+                    blurRadius: 6,
+                    offset: Offset(2, 4),
                   ),
                 ]
                 : [],
       ),
-      child: Text(word, style: TextStyle(fontSize: 16)),
+      child: Text(
+        word,
+        style: TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+          color: Colors.black87,
+        ),
+      ),
     );
   }
 }
